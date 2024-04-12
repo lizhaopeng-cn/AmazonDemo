64cead830bfd5e4de4a96536210099d265fbb79c
lizhaopeng <lizhaopeng.cn@gmail.com>
2024-04-12 18:34:46 +0800

    Amazon Login

--- app/src/main/java/com/lzp/amazondemo/MainActivity.java
+++ app/src/main/java/com/lzp/amazondemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.lzp.amazondemo;
 
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_amazon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginWithAmazonActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
